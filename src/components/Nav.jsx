0bd0e89f4f8e2e7d9aace5199f7167b1a7426568
fatema <fatema1992xx@gmail.com>
2024-07-04 14:39:51 +0300

    Created Nav and Home pages

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+const Nav = () => {
+  return (
+    <header>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </header>
+  )
+}
+
+export default Nav
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Nav from '../components/Nav'
+
+const Home = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  )
+}
+export default Home

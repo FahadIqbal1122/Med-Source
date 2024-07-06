f5c7942c0e280a7e6949c4c5757ffdb30a96e53a
fatemaajmi <121151845+fatemaajmi@users.noreply.github.com>
2024-07-07 01:24:50 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,63 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import React, { useState, useEffect } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import './App.css';
 import Nav from './components/Nav'
-import Categories from './components/Categories'
-import Offers from './components/Offers'
-import Brands from './components/Brands'
+import Home from './pages/Home';
+import About from './pages/About';
+import Category from './pages/Category';
+import Business from './pages/Business';
+import Coupon from './pages/Coupon';
+import Detail from './pages/Detail';
+import Review from './pages/Review';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+
 const App = () => {
+  const [user, setUser] = useState(null)
+  const [auth, setAuth] = useState(false);
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setAuth(true);
+    }
+  }, []);
+  const handleLogOut = () => {
+    setUser(null)
+    localStorage.clear()
+  }
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
   return (
-    <div>
-      <body>
-        <header>
-          <Nav />
-        </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} exact />
-            <Route path="/Categories" element={<Categories />} exact />
-            <Route path="/Offers" element={<Offers />} exact />
-            <Route path="/Brands" element={<Brands />} exact />
-          </Routes>
-        </main>
-      </body>
+    <div className="App">
+      <div className="space"></div>
+      <Nav />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/categories" element={<Category />} />
+          <Route path="/categories/:category" element={<Business />} />
+          <Route path="/categories/:category/:id/coupons" element={<Coupon />} />
+          <Route path="/categories/:category/:id/coupons/:id/details" element={<Detail />} />
+          <Route path="/categories/:category/:id/coupons/:id/details/:id/review" element={<Review />} />
+          <Route path="/signin" element={<SignIn setAuth={setAuth} />} />
+          <Route path="/signup" element={<SignUp setAuth={setAuth}  /> } />
+          <Route path="/dashboard" element={auth ? <h2>Dashboard</h2> : <Navigate to="/signin" />} />
+        </Routes>
+      </main>
     </div>
   )
 }
 
-export default App
+export default App;

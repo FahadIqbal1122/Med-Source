3b78e4fb42d59b9282cea94ca9614be561a15e1d
MaryamAlmutawa9 <mariamalmutawa5@gmail.com>
2024-07-08 11:57:25 +0300

    RegisterUser function

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -9,3 +9,12 @@ export const SignInUser = async (data) => {
   }
 }
 
+export const RegisterUser = async (data) => {
+  try {
+    const res = await Client.post('/auth/register', data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+

96e9c63bf0443ead583fce8cf55b093471d8b36a
OsamaMohammad61 <osamamohammad343@gmail.com>
2024-07-08 15:55:04 +0300

    Done with creating the request_products

--- src/pages/RequestMed.jsx
+++ src/pages/RequestMed.jsx
@@ -1,58 +1,83 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 const RequestMed = () => {
   const [patientName, setPatientName] = useState('')
-  const [MedicineName, setMedicineName] = useState('')
+  const [medicines, setMedicines] = useState([])
+  const [selectedMedicine, setSelectedMedicine] = useState('')
   const [requestDetails, setRequestDetails] = useState('')
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
     console.log('Form submitted with:', {
       patientName,
-      MedicineName,
+
       requestDetails
     })
+    const sent = await axios.post(`http://localhost:5000/request`, {
+      user_id: patientName,
+      product_ids: selectedMedicine
+    })
+    console.log(sent)
     setPatientName('')
     setMedicineName('')
     setRequestDetails('')
   }
-
+  useEffect(() => {
+    const get_products = async () => {
+      const response = await axios.get(`http://localhost:5000/products`)
+      const availableMedicines = response.data.filter(
+        (medicine) => !medicine.availability
+      )
+      console.log(availableMedicines)
+      setMedicines(availableMedicines)
+    }
+    get_products()
+  }, [])
+  console.log(selectedMedicine)
   return (
-    <>
-      <div className="request-med">
-        <h2>Request Medical Information</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="patientName">Patient Name:</label>
-            <input
-              type="text"
-              id="patientName"
-              value={patientName}
-              onChange={(e) => setPatientName(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-group"> 
-            <label htmlFor="MedicineName">Medicine Name:</label>
-            <input
-              id="MedicineName"
-              value={MedicineName}
-              onChange={(e) => setMedicineName(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="requestDetails">Request Details:</label>
-            <textarea
-              id="requestDetails"
-              value={requestDetails}
-              onChange={(e) => setRequestDetails(e.target.value)}
-              required
-            />
-          </div>
-          <button type="submit" className="submit-button">Submit Request</button>
-        </form>
-      </div>
-    </>
+    <div className="request-med">
+      <h2>Request Medical Information</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="patientName">Patient Name:</label>
+          <input
+            type="number"
+            id="patientName"
+            value={patientName}
+            onChange={(e) => setPatientName(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="MedicineName">Medicine Name:</label>
+          <select
+            id="medicine"
+            value={selectedMedicine}
+            onChange={(e) => setSelectedMedicine(e.target.value)}
+            required
+          >
+            <option value="">Select a medicine</option>
+            {medicines.map((medicine) => (
+              <option key={medicine.id} value={medicine.id}>
+                {medicine.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="requestDetails">Request Details:</label>
+          <textarea
+            id="requestDetails"
+            value={requestDetails}
+            onChange={(e) => setRequestDetails(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className="submit-button">
+          Submit Request
+        </button>
+      </form>
+    </div>
   )
 }
 

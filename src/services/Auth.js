3eebed69c8576a8635ba9f273b9cb61f6ebf8629
MaryamAlmutawa9 <mariamalmutawa5@gmail.com>
2024-07-08 11:56:49 +0300

    SignInUser function

---
+++ src/services/Auth.js
@@ -0,0 +1,11 @@
+import Client from './api'
+export const SignInUser = async (data) => {
+  try {
+    const res = await Client.post('/auth/login', data)
+    localStorage.setItem('token', res.data.token)
+    return res.data.user
+  } catch (error) {
+    throw error
+  }
+}
+

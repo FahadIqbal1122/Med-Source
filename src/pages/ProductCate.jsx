10e6937a7b2062a506aebf387bd7a16920840a22
MaryamAlmutawa9 <mariamalmutawa5@gmail.com>
2024-07-10 12:55:44 +0300

    useEffect function in ProductCate

---
+++ src/pages/ProductCate.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useNavigate } from "react-router-dom"
+import axios from 'axios';
+import ProductCard from '../components/ProductCard';
+
+
+const Productcate = () => {
+  const { category } = useParams()
+  const [products, setProducts] = useState([])
+  const navigate = useNavigate()
+
+
+
+  useEffect(() => {
+    const getAllProducts = async () => {
+      try {
+        const res = await axios.get(`http://localhost:5000/products`);
+        const filteredProducts = res.data.filter(product => product.category === category);
+        setProducts(filteredProducts);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    getAllProducts();
+  }, [category]);
+
+  return (
+    
+  );
+};
+
+export default Productcate;

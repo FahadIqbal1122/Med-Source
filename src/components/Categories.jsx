4f433841e8cd897ab9782c6c1db3f51dfd79d268
MaryamAlmutawa9 <mariamalmutawa5@gmail.com>
2024-07-10 13:07:07 +0300

    add handleNavigate for all cate

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -14,25 +14,25 @@ const Categories = () => {
       <div className="Category-container">
         <div className="Category-container1">
           <section>
-            <button onClick={() => handleSubmit('vitamins')}>
+            <button onClick={() => handleNavigate('vitamins')}>
               {' '}
               <img width={500} src="../public/images/Vitamins.png" />
             </button>
           </section>
           <section>
-            <button onClick={() => handleSubmit('minerals')}>
+            <button onClick={() => handleNavigate('minerals')}>
               <img width={500} src="../public/images/minerals.png" />
             </button>
           </section>
           <section>
-            <button onClick={() => handleSubmit('supplements')}>
+            <button onClick={() => handleNavigate('supplements')}>
               <img width={500} src="../public/images/supplements.png" />
             </button>
           </section>
         </div>
         <div className="Category-container2">
           <section>
-            <button onClick={() => handleSubmit('Common-Conditions')}>
+            <button onClick={() => handleNavigate('Common-Conditions')}>
               <img width={500} src="../public/images/Common-Conditions.png" />
             </button>
           </section>
@@ -42,24 +42,24 @@ const Categories = () => {
             </button>
           </section>
           <section>
-            <button onClick={() => handleSubmit('Bath-Shower')}>
+            <button onClick={() => handleNavigate('Bath-Shower')}>
               <img width={500} src="../public/images/Bath-Shower.png" />
             </button>
           </section>
         </div>
         <div className="Category-container3">
           <section>
-            <button onClick={() => handleSubmit('Baby-Skin')}>
+            <button onClick={() => handleNavigate('Baby-Skin')}>
               <img width={500} src="../public/images/Baby-Skin.png" />
             </button>
           </section>
           <section>
-            <button onClick={() => handleSubmit('Health-Accessories')}>
+            <button onClick={() => handleNavigate('Health-Accessories')}>
               <img width={600} src="../public/images/Health-Accessories.png" />
             </button>
           </section>
           <section>
-            <button onClick={() => handleSubmit('Hair')}>
+            <button onClick={() => handleNavigate('Hair')}>
               <img width={500} src="../public/images/Hair.png" />
             </button>
           </section>
